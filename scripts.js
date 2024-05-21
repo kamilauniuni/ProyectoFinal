883acfbc8b05ccbd87eabb95faa4a487f4b6ee55
kamila <kamilauniuni@gmail.com>
2024-05-20 22:18:09 -0500

    Js y agregación de imagenes

---
+++ scripts.js
@@ -0,0 +1,26 @@
+// scripts.js
+document.addEventListener("DOMContentLoaded", function() {
+    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    const rootElement = document.documentElement;
+
+    function handleScroll() {
+        const scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+        if (rootElement.scrollTop / scrollTotal > 0.2) {
+            
+            scrollToTopBtn.style.display = "block";
+        } else {
+           
+            scrollToTopBtn.style.display = "none";
+        }
+    }
+
+    function scrollToTop() {
+        rootElement.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
+
+    scrollToTopBtn.addEventListener("click", scrollToTop);
+    document.addEventListener("scroll", handleScroll);
+});
