caf4db2b9d96f67996093441f53f952b56a15434
Deiby Buitrago <kamilauniuni@gmail.com>
2024-05-27 17:20:47 -0500

    contactanos

--- scripts.js
+++ scripts.js
@@ -24,3 +24,27 @@ document.addEventListener("DOMContentLoaded", function() {
     scrollToTopBtn.addEventListener("click", scrollToTop);
     document.addEventListener("scroll", handleScroll);
 });
+
+
+$(document).ready(function(){
+    $(".testimonial-carousel").owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+});
